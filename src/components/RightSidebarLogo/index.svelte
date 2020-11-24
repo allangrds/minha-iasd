d59c620ff0f2e407cb66b901d26ba2664a326b8a
Allan Ramos <allangrds@gmail.com>
2020-11-24 19:27:18 -0300

    feat(rightsidebarlogo): add new component

---
+++ src/components/RightSidebarLogo/index.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Logo from '~/assets/images/logo_white_simple.svg'
+  import './style.css'
+</script>
+
+<aside class="seventh-day">
+  <div class="seventh-day-2">
+    <div class="seventh-day-3">
+      <img class="logo" src={Logo} alt="Igreja Adventista do Sétimo Dia" />
+    </div>
+  </div>
+</aside>
