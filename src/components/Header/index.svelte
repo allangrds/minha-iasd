d42da3e23159ef3505967c1707c01144f62c7007
Allan Ramos <allangrds@gmail.com>
2020-11-30 22:18:55 -0300

    feat(header): add new component

---
+++ src/components/Header/index.svelte
@@ -0,0 +1,157 @@
+<script>
+  import './style.css'
+</script>
+
+
+<header class="header">
+  <div class="container">
+    <div>
+      <h1 class="church-name">
+        Igreja Adventista do Sétimo Dia
+      </h1>
+      <h2 class="church-name-sub">
+        Belém
+      </h2>
+    </div>
+    <div class="mobile-navigation">
+      <div class="menu">
+        <label
+          class="icon"
+          for="toggle"
+        >
+          &#9776;
+        </label>
+        <input
+          class="input"
+          type="checkbox"
+          id="toggle"
+        />
+        <span class="text">
+          Menu
+        </span>
+        <nav class="navigation">
+          <ul class="institutional-list">
+            <li class="item">
+              <a
+                class="link"
+                href="https://www.adventistas.org/pt/institucional/"
+                target="_blank"
+              >
+                institucional
+              </a>
+            </li>
+            <li class="item">
+              <a
+                class="link"
+                href="https://noticias.adventistas.org/pt/"
+                target="_blank"
+              >
+                notícias
+              </a>
+            </li>
+            <li class="item">
+              <a
+                class="link"
+                href="https://videos.adventistas.org/pt/"
+                target="_blank"
+              >
+                vídeos
+              </a>
+            </li>
+          </ul>
+          <ul class="site-list">
+            <li class="item">
+              <a
+                class="link"
+                href="#"
+              >
+                início
+              </a>
+            </li>
+            <li class="item">
+              <a
+                class="link"
+                href="#"
+              >
+                ao vivo
+              </a>
+            </li>
+            <li class="item">
+              <a
+                class="link"
+                href="#"
+              >
+                onde estamos
+              </a>
+            </li>
+            <li class="item">
+              <a
+                class="link"
+                href="#"
+              >
+                fale conosco
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div class="desktop-navigation">
+      <nav class="institutional-navigation">
+        <ul class="list">
+          <li class="item">
+            <a
+              class="link"
+              href="https://www.adventistas.org/pt/institucional/"
+              target="_blank"
+            >
+              institucional
+            </a>
+          </li>
+          <li class="item">
+            <a
+              class="link"
+              href="https://noticias.adventistas.org/pt/"
+              target="_blank"
+            >
+              notícias
+            </a>
+          </li>
+          <li class="item">
+            <a
+              class="link"
+              href="https://videos.adventistas.org/pt/"
+              target="_blank"
+            >
+              vídeos
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <nav class="site-navigation">
+        <ul class="list">
+          <li class="item">
+            <a href="#" class="link">
+              Início
+            </a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">
+              Ao vivo
+            </a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">
+              Onde estamos
+            </a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">
+              Fale conosco
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
