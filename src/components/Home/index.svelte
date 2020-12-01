b952c0956e32efd9a396e787430a2e2753c6b9b3
Allan Ramos <allangrds@gmail.com>
2020-11-30 22:19:42 -0300

    feat(home): add new container

---
+++ src/components/Home/index.svelte
@@ -0,0 +1,23 @@
+<script>
+  import './style.css'
+
+  import RightSidebarLogo from '~/components/RightSidebarLogo/index.svelte'
+  import Header from '~/components/Header/index.svelte'
+  import Content from '~/components/Content/index.svelte'
+  import Wrapper from '~/components/Wrapper/index.svelte'
+  import Main from '~/components/Main/index.svelte'
+  import Container from '~/components/Container/index.svelte'
+</script>
+
+<Wrapper>
+  <Content>
+    <Header />
+    <Main>
+      <div class="header-img"></div>
+      <Container>
+        <p>asdasdasdasdasdasdsa</p>
+      </Container>
+    </Main>
+  </Content>
+  <RightSidebarLogo />
+</Wrapper>
