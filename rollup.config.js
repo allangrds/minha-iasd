1efe806bd4a3aa35f317d972b7e7c951a0e52e11
Allan Ramos <allangrds@gmail.com>
2020-12-08 23:32:50 -0300

    feat(rollup): add css modules

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import cssModules from 'svelte-preprocess-cssmodules';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -41,7 +42,10 @@ export default {
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
-			}
+			},
+			preprocess: [
+				cssModules(),
+			]
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
